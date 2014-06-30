d36878b96205292b44861bfc3c12c8ee317424ac
László Németh <nemeth@numbertext.org>
2014-06-30 10:22:51 +0000

    add missing #include <stdio.h> to hyphen.h

--- hyphen.h
+++ hyphen.h
@@ -55,6 +55,8 @@
 extern "C" {
 #endif /* __cplusplus */
 
+#include <stdio.h>
+
 typedef struct _HyphenDict HyphenDict;
 typedef struct _HyphenState HyphenState;
 typedef struct _HyphenTrans HyphenTrans;
